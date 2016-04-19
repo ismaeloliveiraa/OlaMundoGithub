fba521b4e6426bbc82523560537b12f8a329040e
Ismael Oliveira <ismael.o.m.s@gmail.com>
2016-04-19 11:41:15 -0300

    Primeiro commit com Eclipse

---
+++ OlaMundoGithub/src/br/com/OlaMundoGithub/OlaMundo.java
@@ -0,0 +1,10 @@
+package br.com.OlaMundoGithub;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		//Versionando pelo Eclipse
+		System.out.println("Ola Mundo GitHub");
+	}
+
+}
